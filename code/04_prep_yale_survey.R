ad7915a5e531e69667713d1574b4f298dd770f72
gustavpirich <gustav@pirich.at>
2025-04-28 10:20:47 +0200

    upploading stuff

---
+++ code/04_prep_yale_survey.R
@@ -0,0 +1,8 @@
+library(haven)
+library(here)
+
+# Replace 'yourfile.sav' with your file path
+survey <- read_sav(here("data", "CCAM SPSS Data 2008-2023.sav"))
+
+# View the first few rows
+view(survey)
